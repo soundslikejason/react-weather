144d2cf034ab69bd62d64c20f5e40f1f6ccd6820
Jason Leong <soundslikejason@users.noreply.github.com>
2017-02-11 16:54:20 -0800

    adding react-router

--- app/app.jsx
+++ app/app.jsx
@@ -1,19 +1,13 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var objOne = {
-  name: 'Jason',
-  location: 'Los Angeles'
-};
-
-var objTwo = {
-  nationality: 'American',
-  ...objOne
-};
-
-console.log(objTwo);
+var {Route, Router, IndexRoute, hashHistory} = require("react-router");
+var Main = require('Main');
 
 ReactDOM.render(
-  <h1>Boilerplate app</h1>,
+  <Router history={hashHistory}>
+    <Route path="/" component={Main}>
+
+    </Route>
+  </Router>,
   document.getElementById('app')
 );
